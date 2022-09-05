3976be8f29af142a4af120a4f652afe175e8733b
Dhriti Prasanna Paul <52440734+dhritippaul@users.noreply.github.com>
2022-09-06 02:22:25 +0530

    Time: 36 ms (57.21%), Space: 19.2 MB (57.57%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int k = 0 ; 
+        while(k<n){
+            if(nums[k]==0) break;
+            else{
+                k = k+1;
+            }
+        }
+        
+        int i = k;
+        int j = k+1;
+        while(i<n && j<n){
+            if(nums[j]!=0){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+            }
+            j++;
+        }
+        
+    }
+};
